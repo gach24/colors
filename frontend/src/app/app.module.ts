ec648ee40c170bfcc7ea736d7e5367a958346296
German <germanach@educastur.org>
2020-03-27 02:29:26 +0100

    Creados los primeros componentes y rutas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
