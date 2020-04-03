d0e2ca7203bcc256d3df6e1e722673f1baceac8e
geman <germanach@educastur.org>
2020-04-04 01:03:05 +0200

    Modificaciones en express para aceptar el modelo

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,6 +28,47 @@ var dataSchema = new mongoose.Schema({
     type: String, 
     required: [true, "Gender is necesary"] 
   },
+  q01: String,
+  q02: String,
+  q03: String,
+  q04: String,
+  q05: String,
+  q06: String,
+  q07: String,
+  q08: String,
+  q09: String,
+  q10: String,
+  q11: String,
+  q12: String,
+  q13: String,
+  q14: String,
+  q15: String,
+  q16: String,
+  q17: String,
+  q18: String,
+  q19: String,
+  q20: String,
+  q21: String,
+  q22: String,
+  q23: String,
+  q24: String,
+  q25: [String], 
+  q26: [String], 
+  q27: [String], 
+  q28: [String], 
+  q29: [String], 
+  q30: [String], 
+  q31: [String], 
+  q32: [String], 
+  q33: [String], 
+  q34: [String], 
+  q35: [String], 
+  q36: [String], 
+  q37: [String], 
+  q38: [String], 
+  q39: [String], 
+  q40: [String], 
+  q41: [String],  
   created: {
     type: Date, 
     required: true,
@@ -60,32 +101,19 @@ app.use(express.json());
 
 app.post('/new', (req, res, next) => {
 
-  
-
-  
   let poll = new Poll(req.body);
+  console.log(poll);
   poll.save()
     .then(newPoll => res.status(200).json({ok: true, new: newPoll}))
     .catch(err => res.status(500).json({ok: false, message: "No se ha podido insertar en la base de datos" }));
-  
-  /*
-  let t = Number(req.query.temperature) || 0;
-  let h = Number(req.query.humidity) || 0;
-
-  let measure = new Measure({ temperature: t, humidity: h });
-
-  measure.save()
-    .then(newMeasure => res.status(200).json({ok: true, new: newMeasure}))
-    .catch(err => res.status(500).json({ok: false, message: "No se ha podido insertar en la base de datos" }));
-  */
 });
 
 
 app.get('/',  (req, res, next) => {
 
-  Measure.find({}).exec()
+  Poll.find({}).exec()
     .then(data => {
-      res.status(200).json({ok: true, measures: data });
+      res.status(200).json(data);
     })
     .catch(err => res.status(500).json({ok: false, message: "No se ha podido conectar a la base de datos" }));
 
