9b78e87782f376990fb84a8ecfc32c92a968ca10
geman <germanach@educastur.org>
2020-04-03 02:23:50 +0200

    Añadida la lógica javascript

--- frontend-jquery/src/js/scripts.js
+++ frontend-jquery/src/js/scripts.js
@@ -1,26 +1,115 @@
 
-
 $(function () {
 
-    var url = 'http://localhost:8080/new';
+    const colors = [
+        'brown', 
+        'pink', 
+        'grey', 
+        'violet', 
+        'orange', 
+        'yellow', 
+        'black', 
+        'green', 
+        'red', 
+        'gold', 
+        'silver', 
+        'blue',
+        'white'
+    ];
+
+    const url = 'http://localhost:8080/new';
     var poll = {
         email: 'germanach@educastur.org',
         age: 18,
-        gender: 'V'
+        gender: 'V',
+        q01: '',
+        q02: '',
+        q03: '',
+        q04: '',
+        q05: '',
+        q06: '',
+        q07: '',
+        q08: '',
+        q09: '',
+        q10: '',
+        q11: '',
+        q12: '',
+        q13: '',
+        q14: '',
+        q15: '',
+        q16: '',
+        q17: '',
+        q18: '',
+        q19: '',
+        q20: '',
+        q21: '',
+        q22: '',
+        q23: '',
+        q24: '',
+        q25: [],
+        q26: '',
+        q27: '',                
+        q28: '',
+        q29: '',
+        q30: '',
+        q31: '',
+        q32: '',
+        q33: '',
+        q34: '',
+        q35: '',
+        q36: '',
+        q37: '',
+        q38: '',
+        q39: '',
+        q40: '',
+        q41: '',
     };  
    
 
-    // Init seccion 1
-    $('.first i').hide();
-    $('.first .grid div').on('click', function() {
-        $('.first i').hide();
+    /**
+     * Init first seccion 1
+     */ 
+    $('.first-section i').hide();
+    $('.first-section .grid-item').on('click', function() {
+        $('.first-section i').hide();
         $(this).find('i').show();
+        $('.first-section .grid-item').removeClass('selected');
+        $(this).addClass('selected');
+        poll['q01'] = $(this).attr('data-color');
+        console.log(poll);
     });
 
+    /**
+     * Init second section
+     */
+    $('.second-section input:radio').on('change', function() {
+        poll[$(this)[0].name] = $(this)[0].value;        
+    });
+    /*
+    $('.second-section tr').each(function(i, element) {
+        console.log(i);
+        console.log(element);
+
+    });*/
+
+
     // Init section 3
-    $('.thrid i').hide();
-    $('.thrid .options div').on('click', function() {
-        $(this).find('i').show();
+    $('.thrid-section i').hide();
+    $('.thrid-section .options div').on('click', function() {
+        let colors = poll[$(this).parent()[0].id]; 
+        let color  = $(this).attr('data-color');
+        let position = colors.indexOf(color)
+        if ( position < 0) {
+            $(this).addClass('selected'); 
+            $(this).find('i').show();
+            colors.push(color);
+            console.log(poll);
+        } else {
+            $(this).removeClass('selected');
+            $(this).find('i').hide();
+            colors.splice(position, 1);
+            console.log(poll);
+        }        
     });
 
     $('button').on('click', function() {
